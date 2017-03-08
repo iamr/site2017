f822348d0c002331206e88cf23ad27583cb554b2
i am r <ik@i-am-r.nl>
2017-03-08 16:13:59 +0100

    Test site svn 2017

---
+++ clef-test/config.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+Before you begin, you'll need to add the following table to your database.
+
+CREATE TABLE users (
+    clef_id VARCHAR(256),
+    name VARCHAR(256),
+    logged_out_at INTEGER
+);
+*/
+
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'clef_test');
+define('CLEF_BASE_URL', 'https://clef.io/api/v1/');
+define('APP_ID', '562306be5c59cc3f2da25095c05da670');
+define('APP_SECRET', '9fd4e0d1e240e6f95b20a6223c3edbfc');
